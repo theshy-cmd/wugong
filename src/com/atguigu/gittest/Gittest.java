318c5c7e0aad01ff7b9016eb11fcfee53093a124
theshy-cmdd <3293798151@qq.com>
2019-11-07 10:43:48 +0800

    test6

--- src/com/atguigu/gittest/Gittest.java
+++ src/com/atguigu/gittest/Gittest.java
@@ -4,6 +4,6 @@ public class Gittest {
 	public static void main(String[] args) {
 		System.out.println("HELLO GIT");
 		System.out.println("HI");
-		System.out.println("test3");
+		System.out.println("test6");
 	}
 }
